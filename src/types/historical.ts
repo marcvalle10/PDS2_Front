14cb5e212320a9745a8271e267057dd723200f0d
jessica <jessymontano@hotmail.com>
2025-11-24 18:43:26 -0700

    agregar tipo para archivos

--- src/types/historical.ts
+++ src/types/historical.ts
@@ -1,21 +1,25 @@
 // Tipos específicos para los datos históricos
 export interface HistoricalRecord {
-	id: string;
-	matricula: string;
-	expediente: string;
-	nombre: string;
-	email: string;
-	estadoAcademico: 'Activo' | 'Inactivo';
-	nivelIngles: string;
-	estadoAcreditacion: string;
-	kpi: string;
-	planEstudios: string;
-	creditos: string;
-	sexo: string;
-	nip: string;
-	fechaNacimiento: string;
-	tipoAlumno: string;
-	promedioGeneral: string;
-	horarioPerido: string;
-	acciones: string;
+  id: number;
+  matricula: string;
+  expediente: string;
+  nombre: string;
+  email: string;
+  estadoAcademico: "ACTIVO" | "INACTIVO";
+  nivelIngles: string;
+  planEstudios: string;
+  creditos: string;
+  sexo: string;
+  fechaNacimiento: string;
+  tipoAlumno: string;
+  promedioGeneral: string;
+  horarioPerido: string;
+  acciones: string;
+}
+
+export interface FileHistoryRecord {
+  id: number;
+  date: string;
+  filename: string;
+  status: "ERROR" | "CANCELADO" | "COMPLETADO" | "PENDIENTE";
 }
